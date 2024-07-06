77f242f1f94ecad2eac7c8bd91db70cd35d8be8b
Viktoria Grushko <grushko13vika@gmail.com>
2024-07-06 19:40:00 +0300

    changes 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
-const btn = document.querySelector('.menu-btn');
-const inner = document.querySelector('.header__inner');
+const btn = document.querySelector(".menu-btn");
+const inner = document.querySelector(".header__inner");
 
-btn.addEventListener('click', ()=>{
-    inner.classList.toggle('menu-open');
+btn.addEventListener("click", () => {
+  inner.classList.toggle("menu-open");
+});
+
+$(document).ready(function () {
+  $(".complete__img").slick({
+    arrows: false,
+    infinity: true,
+    initialSlide: 1,
+    slidesToShow: 1.5,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1.2,
+          slidesToScroll: 1,
+          infinite: false,
+          centerMode: true,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1.2,
+          slidesToScroll: 1,
+          infinite: false,
+          centerMode: false,
+          variableWidth: true,
+        },
+      },
+    ],
+  });
 });
